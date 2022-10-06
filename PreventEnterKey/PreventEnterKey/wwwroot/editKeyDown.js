40329e10afbff4079826c01d3e4ce0f44a0b57db
Vignesh Natarajan <vignesh.natarajan@syncfusion.com>
2022-10-06 12:16:46 +0530

    sample

---
+++ PreventEnterKey/PreventEnterKey/wwwroot/editKeyDown.js
@@ -0,0 +1,7 @@
+﻿function editKeyDown(id) {
+    document.getElementById(id).addEventListener("keydown", function (e) {
+        if (e.key == "Enter") {
+            e.stopPropagation();
+        }
+    });
+}
